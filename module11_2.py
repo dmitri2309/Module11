492965b87e51e549c56c2690c479291194a9bdd5
dmitri2309 <kerzhimanov@gmail.com>
2024-11-19 13:41:14 +0300

    Add files via upload

---
+++ module11_2.py
@@ -0,0 +1,31 @@
+# Домашнее задание по теме "ИНТРОСПЕКЦИЯ"
+
+from PIL import Image
+import inspect
+
+
+class Folder:
+    def __init__(self, name):
+        self.name = name
+
+    def image_show(self):
+        with Image.open(self.name) as img:
+            img.load()
+
+
+image1 = Folder(r"Design2\crop_replicated_image3.png")
+
+
+def introspection_info(obj):
+    res = {}
+    res['type'] = type(obj)
+    for attr_name in dir(obj):
+        attr = getattr(obj, attr_name)
+        res['attributes:'] = attr
+    methods_ = [m[0] for m in inspect.getmembers(obj, predicate=inspect.ismethod)]
+    res['methods:'] = methods_
+    res['module:'] = inspect.getmodule(obj)
+    print(res)
+
+
+introspection_info(image1)
